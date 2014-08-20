8d7f92ba21a7b7f8e411d321e8c974f2861be446
John Weis <weis.john@gmail.com>
2014-08-20 15:38:18 -0400

    initial sample of getting button presses to work

---
+++ explorer.js
@@ -0,0 +1,62 @@
+var usb = require('usb');
+var lo = _ = require('lodash');
+var async = require('async');
+
+var repl = require('repl');
+
+var devices = [];
+
+  // Guitar Hero X-plorer:
+  // Product ID:   0x4748                           18248
+  // Vendor ID:    0x1430  (RedOctane)              
+  // Version:  31.22
+  // Serial Number:    0ADD7D8
+  // Speed:    Up to 12 Mb/sec
+  // Manufacturer: RedOctane Inc(c)2006 
+  // Location ID:  0x14100000 / 3
+  // Current Available (mA):   500
+  // Current Required (mA):    500
+
+
+usb.setDebugLevel(1);
+
+var i = 0;
+_.each(usb.getDeviceList(), function(device) {
+
+    if (device.deviceDescriptor.idProduct !== 18248) return; 
+    devices.push(device);
+
+    // TODO: call open somewhere else?
+
+    device.open();
+
+    _.each(device.interfaces, function(inter) {
+
+        inter.claim();
+
+        _.each(inter.endpoints, function(end) {
+            
+
+            if (end.direction !== "in") return;
+            console.log()
+            console.log(end.descriptor)
+
+            end.startStream(7, end.descriptor.wMaxPacketSize);
+
+            end.on('data', function(d) {
+                console.log(d);
+            })
+
+        });
+
+
+
+    });
+
+    // device.close();
+
+});
+
+
+var local = repl.start(" ಠ_ಠ > "); 
+local.context.devices = devices;
