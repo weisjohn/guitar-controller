ccdbd8489085e370e2e859ef9e0bfe207738327f
John Weis <weis.john@gmail.com>
2020-02-14 23:47:56 -0500

    examples: DRY up the basic using new definitions

--- examples/basic.js
+++ examples/basic.js
@@ -1,4 +1,5 @@
-var controllers = require('../index')();
+var guitar_controller = require('../index');
+var controllers = guitar_controller();
 
 if (!controllers[0]) throw new Error("No controller found");
 var controller = controllers[0];
@@ -7,14 +8,13 @@ function log(val) {
     return function(arg) { console.log(val, typeof arg !== "undefined" ? arg : "") }
 }
 
-var buttons = ["green", "red", "yellow", "blue", "orange", "start", "back", "up", "down", "left", "right", "xbox"]
-buttons.forEach(function(button) {
-    var press = button + ".press";
-    var release = button + ".release";
-    controller.on(press, log(press));
-    controller.on(release, log(release));
+Object.keys(guitar_controller.buttons).forEach(function(button) {
+  var press = button + ".press";
+  var release = button + ".release";
+  controller.on(press, log(press));
+  controller.on(release, log(release));
 });
 
-controller.on("x", log("x"));
-controller.on("y", log("y"));
-controller.on("whammy", log("whammy"));
+Object.keys(guitar_controller.ranges).forEach(function(range) {
+    controller.on(range, log(range));
+});
